ca3d1703098400102571a85894c55ffd8a499a2c
Юрий <145429474+yuriIosDev@users.noreply.github.com>
2024-01-17 20:50:37 +0300

    Initial Commit

---
+++ Modul2dz3/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Modul2dz3
+//
+//  Created by Юрий Кузнецов on 17.01.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
